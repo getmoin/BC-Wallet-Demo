640c93627ead8ef5eb0cf173ad283aa7aa7fa038
sanderPostma <spostma@sphereon.com>
2025-03-24 13:39:40 +0100

    chore: prettier & lint fixes

--- eslint.config.js
+++ eslint.config.js
@@ -47,7 +47,7 @@ module.exports = [
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-use-before-define': ['error', { functions: false, classes: false, variables: true }],
       '@typescript-eslint/explicit-member-accessibility': 'error',
-      'no-console': 'error',
+      'no-console': 'warn',
       '@typescript-eslint/ban-ts-comment': 'warn',
       '@typescript-eslint/consistent-type-imports': 'error',
       'import/no-cycle': 'error',
